ba9bbee754941c5c2fbdbbd21d09911acc67291a
Abby <abigailjwinn@gmail.com>
2023-05-24 09:03:06 -0500

    Creation of app

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddUser from "./components/users/AddUser.js"
+function App() {
+  return (
+    <div>
+      <AddUser></AddUser>
+    </div>
+  );
+}
+export default App;
